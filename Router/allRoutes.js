a687cf79d55073d5a39d118d648dbb3b4061191a
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-05-16 09:42:34 +0200

    Ajout des pages Vos Réservations + Ma Réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/account", "Mon Compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de Mot de Passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Mes Réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
