bce28bd6101abcf7c5a65b25e6012772dab64ea7
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-05-16 09:27:34 +0200

    Gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon Compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de Mot de Passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Mes Réservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
