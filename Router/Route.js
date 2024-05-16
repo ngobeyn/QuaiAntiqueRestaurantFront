0191bb6aaafd7a859fec02cb353d2686c3468604
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-05-16 14:04:55 +0200

    Update Routage

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,17 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+/*
+[] -> Tout le monde peut y accéder
+["disconnected"] -> Réservé aux utilisateurs déconnectés
+["client"] -> Réservé aux utilisateurs avec le rôle client
+["admin"] -> Réservé aux utilisateurs avec le rôle admin
+["admin", "client"] -> Réservé aux utilisateurs avec le rôle client OU admin
+*/
---
+++ js/auth/signin.js
@@ -0,0 +1,25 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    // Ici, il faudra appeler l'API pour vérifier les crédentials en BDD
+
+    if(mailInput.value == 'test@mail.com' && passwordInput.value == "123"){
+       
+        // Ici, il faudra récupérer le vrai token
+        const token = "Ceci est un token de test";
+        setToken(token);
+
+        // Placer ce token en cookie
+
+        setCookie(RoleCookieName, "client", 7);
+        window.location.replace("/");
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
